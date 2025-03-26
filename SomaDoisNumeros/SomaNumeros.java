4a597e6aaa90d8ca36ea2fa8efbb67704da04086
AlexandreIsaque <blake98124@gmail.com>
2025-03-26 15:12:13 -0300

    Soma de dois numeros

---
+++ SomaDoisNumeros/SomaNumeros.java
@@ -0,0 +1,11 @@
+package SomaDoisNumeros;
+
+public class SomaNumeros {
+    public static void main(String[] args) {
+        soma(10, 10);
+    }
+    public static void soma(int numero1, int numero2){
+        int sum = numero1 + numero2;
+        System.out.println("soma: " + sum);
+    }
+}
